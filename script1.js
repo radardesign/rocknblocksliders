baa0edf41bc750711288646dbdc97e23ab53bddf
radar <66486286+radardesign@users.noreply.github.com>
2024-10-28 20:03:17 +0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,11 @@
+const heroSlider = new Swiper('.swiper.heroslider', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
