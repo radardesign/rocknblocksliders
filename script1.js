1111dd6b09da628d95df521bd200eeeb4676de46
radar <66486286+radardesign@users.noreply.github.com>
2024-10-28 20:47:40 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,8 @@
 const heroSlider = new Swiper('.swiper.heroslider', {
   // Optional parameters
-  centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
 
   // Navigation arrows
   navigation: {
