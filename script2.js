f3e4b8edba71ab20cb3537c6401a77a93e1885f5
radar000 <prstbtw@gmail.com>
2024-10-30 21:24:07 +0300

    123

--- script2.js
+++ script2.js
@@ -2,9 +2,6 @@ const blockproj = new Swiper('.swiper.blocproj', {
   // Optional parameters
   slidesPerView: 2,
   slidesPerGroup: 2,
-  grid: {
-    rows: 2,
-  },
   spaceBetween: 8,
   loop: true,
   // Navigation arrows
