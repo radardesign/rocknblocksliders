c30a1ba35b03cc445fdb403293c7e36d2911e82c
radar000 <prstbtw@gmail.com>
2024-10-30 21:21:09 +0300

    123

--- script2.js
+++ script2.js
@@ -2,10 +2,6 @@ const blockproj = new Swiper('.swiper.blocproj', {
   // Optional parameters
   slidesPerView: 2,
   loop: true,
-  effect: "fade",
-  fadeEffect: {
-      crossFade: true
-  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-btn-next1',
