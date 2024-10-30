f77d17fcdfef05cd1fc8d50683f2781f6f40a314
radar000 <prstbtw@gmail.com>
2024-10-30 21:54:06 +0300

    123

--- script2.js
+++ script2.js
@@ -10,6 +10,8 @@ const blockproj = new Swiper('.swiper.blocproj', {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    bulletClass: 'swiper-bullet',
+    bulletActiveClass: 'is-active',
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
