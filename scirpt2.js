88c9d6f2b26fc473b00cfb461963bc4d2e2c52f6
radar <66486286+radardesign@users.noreply.github.com>
2024-10-30 20:24:08 +0300

    Update scirpt2.js

--- scirpt2.js
+++ scirpt2.js
@@ -1,1 +1,15 @@
+const blockproj = new Swiper('.swiper.blocproj', {
+  // Optional parameters
+  slidesPerView: 1,
+  effect: "fade",
+  fadeEffect: {
+      crossFade: true
+  },
+  loop: false,
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
