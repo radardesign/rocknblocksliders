b508b59d18a52b38919b2e5aa82a4a61212d62da
radar <66486286+radardesign@users.noreply.github.com>
2024-10-30 21:03:45 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,11 +1,11 @@
 const blockproj = new Swiper('.swiper.blocproj', {
   // Optional parameters
   slidesPerView: 2,
-  loop: true,
   effect: "fade",
   fadeEffect: {
-    crossFade: true
+      crossFade: true
   },
+  loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-btn-next1',
